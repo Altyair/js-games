6c0debaabffb66d45c93ca1ecc115355789ca6fa
dzhumaniyazovdm <gitlab@xrm.ru>
2024-02-22 13:43:31 +0300

    commit

--- src/app/animations/canvas-2/canvas-2.component.ts
+++ src/app/animations/canvas-2/canvas-2.component.ts
@@ -14,10 +14,7 @@ export class Canvas2Component implements AfterViewInit {
         this.context = this.canvas!.nativeElement.getContext('2d');
 
         const config: any = {
-            smooth: 0.65,
-            sphereRad: 300,
-            bigDotRad: 35,
-            mouseSize: 120,
+            timeValue: 0.003,
         };
 
         let w: number = (this.canvas!.nativeElement.width = innerWidth),
@@ -27,81 +24,99 @@ export class Canvas2Component implements AfterViewInit {
             time1: number = 0;
 
         const startX = w! / 2;
-        const startY = h! / 1.4;
+        const startY = h! / 1.5;
+
+        const bezierPoints = [
+            [
+                [startX, startY],
+                [startX + 300, startY - 200],
+                [startX + 200, startY - 400],
+                [startX, startY - 250],
+            ],
+            [
+                [startX, startY - 250],
+                [startX - 200, startY - 400],
+                [startX - 300, startY - 200],
+                [startX, startY],
+            ],
+        ];
 
         const drawBezierPoints = () => {
             this.context?.beginPath();
             this.context!.lineWidth = 1;
             this.context!.strokeStyle = 'lightgray';
-            this.context?.moveTo(startX, startY);
+            this.context?.moveTo(bezierPoints[0][0][0], bezierPoints[0][0][1]);
 
-            this.context?.lineTo(startX + 300, startY - 200);
-            this.context?.lineTo(startX + 200, startY - 400);
-            this.context?.lineTo(startX, startY - 250);
-
-            this.context?.lineTo(startX - 200, startY - 400);
-            this.context?.lineTo(startX - 300, startY - 200);
-            this.context?.lineTo(startX, startY);
+            for (let i = 0; i < bezierPoints.length; i++) {
+                for (let j = 0; j < bezierPoints[i].length; j++) {
+                    if (j === 0) {
+                        continue;
+                    }
+                    this.context?.lineTo(bezierPoints[i][j][0], bezierPoints[i][j][1]);
+                }
+            }
             this.context!.lineWidth = 0.5;
             this.context!.stroke();
             this.context?.closePath();
         };
-        drawBezierPoints();
+
         const anim = new AnimationCore();
 
-        this.context?.beginPath();
-        this.context!.lineWidth = 5;
-        this.context!.strokeStyle = 'rgb(247, 2, 11)';
-        this.context?.moveTo(startX, startY);
+        const setDefault = () => {
+            time = time1 = 0;
+            this.context?.clearRect(0, 0, w, h);
+            drawBezierPoints();
+            this.context?.beginPath();
+            this.context!.lineWidth = 5;
+            this.context!.strokeStyle = 'rgb(247, 2, 11)';
+            this.context?.moveTo(bezierPoints[0][0][0], bezierPoints[0][0][1]);
+        };
+        setDefault();
 
         const process = () => {
-            let x: any, y: any;
+            let x: number, y: number;
             if (time <= 1) {
                 x =
-                    Math.pow(1 - time, 3) * startX +
-                    3 * Math.pow(1 - time, 2) * time * (startX + 300) +
-                    3 * (1 - time) * Math.pow(time, 2) * (startX + 200) +
-                    Math.pow(time, 3) * startX;
+                    Math.pow(1 - time, 3) * bezierPoints[0][0][0] +
+                    3 * Math.pow(1 - time, 2) * time * bezierPoints[0][1][0] +
+                    3 * (1 - time) * Math.pow(time, 2) * bezierPoints[0][2][0] +
+                    Math.pow(time, 3) * bezierPoints[0][0][0];
                 y =
-                    Math.pow(1 - time, 3) * startY +
-                    3 * Math.pow(1 - time, 2) * time * (startY - 200) +
-                    3 * (1 - time) * Math.pow(time, 2) * (startY - 400) +
-                    Math.pow(time, 3) * (startY - 250);
+                    Math.pow(1 - time, 3) * bezierPoints[0][0][1] +
+                    3 * Math.pow(1 - time, 2) * time * bezierPoints[0][1][1] +
+                    3 * (1 - time) * Math.pow(time, 2) * bezierPoints[0][2][1] +
+                    Math.pow(time, 3) * bezierPoints[0][3][1];
 
-                time += 0.005;
+                time += config.timeValue;
             } else if (time > 1 && time1 <= 1) {
                 x =
-                    Math.pow(1 - time1, 3) * startX +
-                    3 * Math.pow(1 - time1, 2) * time1 * (startX - 200) +
-                    3 * (1 - time1) * Math.pow(time1, 2) * (startX - 300) +
-                    Math.pow(time1, 3) * startX;
+                    Math.pow(1 - time1, 3) * bezierPoints[1][0][0] +
+                    3 * Math.pow(1 - time1, 2) * time1 * bezierPoints[1][1][0] +
+                    3 * (1 - time1) * Math.pow(time1, 2) * bezierPoints[1][2][0] +
+                    Math.pow(time1, 3) * bezierPoints[1][3][0];
                 y =
-                    Math.pow(1 - time1, 3) * (startY - 250) +
-                    3 * Math.pow(1 - time1, 2) * time1 * (startY - 400) +
-                    3 * (1 - time1) * Math.pow(time1, 2) * (startY - 200) +
-                    Math.pow(time1, 3) * startY;
+                    Math.pow(1 - time1, 3) * bezierPoints[1][0][1] +
+                    3 * Math.pow(1 - time1, 2) * time1 * bezierPoints[1][1][1] +
+                    3 * (1 - time1) * Math.pow(time1, 2) * bezierPoints[1][2][1] +
+                    Math.pow(time1, 3) * bezierPoints[1][3][1];
 
-                // let x = Math.pow(1 - time1, 3) * (w / 2) + 3 * Math.pow(1 - time1, 2) * time1 * (w / 2 - 300) + 3 * (1 - time1) * Math.pow(time1, 2) * (w / 2 - 200) + Math.pow(time1, 3) * (w / 2);
-                // let y = Math.pow(1 - time1, 3) * (h / 1.5) + 3 * Math.pow(1 - time1, 2) * time1 * (h / 1.5 - 200) + 3 * (1 - time1) * Math.pow(time1, 2) * (h / 1.5 - 400) + Math.pow(time1, 3) * (h / 1.5 - 270);
-
-                this.context!.strokeStyle = 'rgb(43, 221, 224)';
-
-                time1 += 0.005;
+                time1 += config.timeValue;
             }
 
-            this.context?.lineTo(Math.floor(x), Math.floor(y));
+            this.context?.lineTo(Math.floor(x!), Math.floor(y!));
             this.context!.stroke();
 
-            if (time1 > 1) {
+            if (time > 1 && time1 === 0) {
                 this.context?.clearRect(0, 0, w, h);
-
-                time = time1 = 0;
                 drawBezierPoints();
                 this.context?.beginPath();
-                this.context!.lineWidth = 5;
-                this.context!.strokeStyle = 'rgb(247, 2, 11)';
-                this.context?.moveTo(startX, startY);
-                // anim.stop();
+                this.context!.lineWidth = 10;
+                this.context!.strokeStyle = 'rgb(43, 221, 224)';
+                this.context?.moveTo(bezierPoints[1][0][0], bezierPoints[1][0][1]);
+            }
+
+            if (time1 > 1) {
+                setDefault();
             }
         };
 
